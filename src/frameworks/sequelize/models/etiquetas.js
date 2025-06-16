2368632948fe5de7426ecc3e9213c367f5c413d0
Santy Sepulveda <santysepu@hotmail.com>
2025-06-16 02:54:00 -0300

    arreglo BD

--- src/frameworks/sequelize/models/etiquetas.js
+++ src/frameworks/sequelize/models/etiquetas.js
@@ -4,23 +4,14 @@ const { Model, Sequelize } = _sequelize;
 export default class etiquetas extends Model {
   static init(sequelize, DataTypes) {
   return super.init({
-    id_publicacion: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      references: {
-        model: 'publicaciones',
-        key: 'id_publicacion'
-      }
-    },
     id_interes: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true,
-      references: {
-        model: 'intereses',
-        key: 'id_interes'
-      }
+      primaryKey: true
+    },
+    nombre: {
+      type: DataTypes.STRING(255),
+      allowNull: true
     }
   }, {
     sequelize,
@@ -33,14 +24,6 @@ export default class etiquetas extends Model {
         using: "BTREE",
         fields: [
           { name: "id_interes" },
-          { name: "id_publicacion" },
-        ]
-      },
-      {
-        name: "etiquetas_ibfk_1",
-        using: "BTREE",
-        fields: [
-          { name: "id_publicacion" },
         ]
       },
     ]
--- src/frameworks/sequelize/models/init-models.js
+++ src/frameworks/sequelize/models/init-models.js
@@ -5,10 +5,10 @@ import _albumnes from  "./albumnes.js";
 import _comentarios from  "./comentarios.js";
 import _compartido from  "./compartido.js";
 import _etiquetas from  "./etiquetas.js";
-import _intereses from  "./intereses.js";
 import _perfiles from  "./perfiles.js";
-import _perfiles_interes from  "./perfiles_interes.js";
+import _perfiles_etiquetas from  "./perfiles_etiquetas.js";
 import _publicaciones from  "./publicaciones.js";
+import _publicaciones_etiquetas from  "./publicaciones_etiquetas.js";
 import _seguidores from  "./seguidores.js";
 import _usuarios from  "./usuarios.js";
 
@@ -18,37 +18,37 @@ export default function initModels(sequelize) {
   const comentarios = _comentarios.init(sequelize, DataTypes);
   const compartido = _compartido.init(sequelize, DataTypes);
   const etiquetas = _etiquetas.init(sequelize, DataTypes);
-  const intereses = _intereses.init(sequelize, DataTypes);
   const perfiles = _perfiles.init(sequelize, DataTypes);
-  const perfiles_interes = _perfiles_interes.init(sequelize, DataTypes);
+  const perfiles_etiquetas = _perfiles_etiquetas.init(sequelize, DataTypes);
   const publicaciones = _publicaciones.init(sequelize, DataTypes);
+  const publicaciones_etiquetas = _publicaciones_etiquetas.init(sequelize, DataTypes);
   const seguidores = _seguidores.init(sequelize, DataTypes);
   const usuarios = _usuarios.init(sequelize, DataTypes);
 
   albumnes.belongsToMany(publicaciones, { as: 'id_publicacion_publicaciones', through: album_publicacion, foreignKey: "id_album", otherKey: "id_publicacion" });
-  intereses.belongsToMany(perfiles, { as: 'id_perfil_perfiles', through: perfiles_interes, foreignKey: "id_interes", otherKey: "id_perfil" });
-  intereses.belongsToMany(publicaciones, { as: 'id_publicacion_publicaciones_etiqueta', through: etiquetas, foreignKey: "id_interes", otherKey: "id_publicacion" });
-  perfiles.belongsToMany(intereses, { as: 'id_interes_intereses_perfiles_interes', through: perfiles_interes, foreignKey: "id_perfil", otherKey: "id_interes" });
+  etiquetas.belongsToMany(perfiles, { as: 'id_perfil_perfiles', through: perfiles_etiquetas, foreignKey: "id_interes", otherKey: "id_perfil" });
+  etiquetas.belongsToMany(publicaciones, { as: 'id_publicacion_publicaciones_publicaciones_etiqueta', through: publicaciones_etiquetas, foreignKey: "id_interes", otherKey: "id_publicacion" });
+  perfiles.belongsToMany(etiquetas, { as: 'id_interes_etiqueta', through: perfiles_etiquetas, foreignKey: "id_perfil", otherKey: "id_interes" });
   perfiles.belongsToMany(perfiles, { as: 'id_seguido_perfiles', through: seguidores, foreignKey: "id_seguidor", otherKey: "id_seguido" });
   perfiles.belongsToMany(perfiles, { as: 'id_seguidor_perfiles', through: seguidores, foreignKey: "id_seguido", otherKey: "id_seguidor" });
   perfiles.belongsToMany(publicaciones, { as: 'id_publicacion_publicaciones_compartidos', through: compartido, foreignKey: "id_destinatario", otherKey: "id_publicacion" });
   publicaciones.belongsToMany(albumnes, { as: 'id_album_albumnes', through: album_publicacion, foreignKey: "id_publicacion", otherKey: "id_album" });
-  publicaciones.belongsToMany(intereses, { as: 'id_interes_intereses', through: etiquetas, foreignKey: "id_publicacion", otherKey: "id_interes" });
+  publicaciones.belongsToMany(etiquetas, { as: 'id_interes_etiquetas_publicaciones_etiqueta', through: publicaciones_etiquetas, foreignKey: "id_publicacion", otherKey: "id_interes" });
   publicaciones.belongsToMany(perfiles, { as: 'id_destinatario_perfiles', through: compartido, foreignKey: "id_publicacion", otherKey: "id_destinatario" });
   album_publicacion.belongsTo(albumnes, { as: "id_album_albumne", foreignKey: "id_album"});
   albumnes.hasMany(album_publicacion, { as: "album_publicacions", foreignKey: "id_album"});
-  etiquetas.belongsTo(intereses, { as: "id_interes_interese", foreignKey: "id_interes"});
-  intereses.hasMany(etiquetas, { as: "etiqueta", foreignKey: "id_interes"});
-  perfiles_interes.belongsTo(intereses, { as: "id_interes_interese", foreignKey: "id_interes"});
-  intereses.hasMany(perfiles_interes, { as: "perfiles_interes", foreignKey: "id_interes"});
+  perfiles_etiquetas.belongsTo(etiquetas, { as: "id_interes_etiqueta", foreignKey: "id_interes"});
+  etiquetas.hasMany(perfiles_etiquetas, { as: "perfiles_etiqueta", foreignKey: "id_interes"});
+  publicaciones_etiquetas.belongsTo(etiquetas, { as: "id_interes_etiqueta", foreignKey: "id_interes"});
+  etiquetas.hasMany(publicaciones_etiquetas, { as: "publicaciones_etiqueta", foreignKey: "id_interes"});
   albumnes.belongsTo(perfiles, { as: "id_perfil_perfile", foreignKey: "id_perfil"});
   perfiles.hasMany(albumnes, { as: "albumnes", foreignKey: "id_perfil"});
   comentarios.belongsTo(perfiles, { as: "id_perfil_perfile", foreignKey: "id_perfil"});
   perfiles.hasMany(comentarios, { as: "comentarios", foreignKey: "id_perfil"});
   compartido.belongsTo(perfiles, { as: "id_destinatario_perfile", foreignKey: "id_destinatario"});
   perfiles.hasMany(compartido, { as: "compartidos", foreignKey: "id_destinatario"});
-  perfiles_interes.belongsTo(perfiles, { as: "id_perfil_perfile", foreignKey: "id_perfil"});
-  perfiles.hasMany(perfiles_interes, { as: "perfiles_interes", foreignKey: "id_perfil"});
+  perfiles_etiquetas.belongsTo(perfiles, { as: "id_perfil_perfile", foreignKey: "id_perfil"});
+  perfiles.hasMany(perfiles_etiquetas, { as: "perfiles_etiqueta", foreignKey: "id_perfil"});
   publicaciones.belongsTo(perfiles, { as: "id_perfil_perfile", foreignKey: "id_perfil"});
   perfiles.hasMany(publicaciones, { as: "publicaciones", foreignKey: "id_perfil"});
   seguidores.belongsTo(perfiles, { as: "id_seguidor_perfile", foreignKey: "id_seguidor"});
@@ -61,8 +61,8 @@ export default function initModels(sequelize) {
   publicaciones.hasMany(comentarios, { as: "comentarios", foreignKey: "id_publicacion"});
   compartido.belongsTo(publicaciones, { as: "id_publicacion_publicacione", foreignKey: "id_publicacion"});
   publicaciones.hasMany(compartido, { as: "compartidos", foreignKey: "id_publicacion"});
-  etiquetas.belongsTo(publicaciones, { as: "id_publicacion_publicacione", foreignKey: "id_publicacion"});
-  publicaciones.hasMany(etiquetas, { as: "etiqueta", foreignKey: "id_publicacion"});
+  publicaciones_etiquetas.belongsTo(publicaciones, { as: "id_publicacion_publicacione", foreignKey: "id_publicacion"});
+  publicaciones.hasMany(publicaciones_etiquetas, { as: "publicaciones_etiqueta", foreignKey: "id_publicacion"});
   perfiles.belongsTo(usuarios, { as: "id_usuario_usuario", foreignKey: "id_usuario"});
   usuarios.hasMany(perfiles, { as: "perfiles", foreignKey: "id_usuario"});
 
@@ -72,10 +72,10 @@ export default function initModels(sequelize) {
     comentarios,
     compartido,
     etiquetas,
-    intereses,
     perfiles,
-    perfiles_interes,
+    perfiles_etiquetas,
     publicaciones,
+    publicaciones_etiquetas,
     seguidores,
     usuarios,
   };
